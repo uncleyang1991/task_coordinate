217295d666dc2b7a0faac684d051f62ec987c28a
yanghaobo <yanghaobo@boco.com.cn>
2018-04-13 15:52:58 +0800

    建立任务协作项目

---
+++ src/main/java/club/yanghaobo/TaskCoordinateApplication.java
@@ -0,0 +1,12 @@
+package club.yanghaobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCoordinateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCoordinateApplication.class, args);
+	}
+}
---
+++ src/test/java/club/yanghaobo/task_coordinate/TaskCoordinateApplicationTests.java
@@ -0,0 +1,16 @@
+package club.yanghaobo.task_coordinate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskCoordinateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
