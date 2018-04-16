0db906b1ae0d6d608de6d196baecf3d0c3eeea61
UncleYang <yangneko@163.com>
2018-04-16 22:37:03 +0800

    2018年4月16日22:36:55

--- src/main/java/club/yanghaobo/controller/TaskController.java
+++ src/main/java/club/yanghaobo/controller/TaskController.java
@@ -2,12 +2,16 @@ package club.yanghaobo.controller;
 
 import club.yanghaobo.entity.DataTableResult;
 import club.yanghaobo.service.ITaskService;
+import club.yanghaobo.tool.IdTool;
 import club.yanghaobo.tool.JsonTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Map;
 
 @RestController
@@ -18,14 +22,35 @@ public class TaskController {
     private ITaskService taskService;
 
     @RequestMapping("/list.do")
-    public String list(@RequestParam Map<String,Object> params){
-        if(params.get("start") == null){
-            params.put("start","0");
+    public String list(@RequestParam Map<String, Object> params) {
+        if (params.get("start") == null) {
+            params.put("start", "0");
         }
-        if(params.get("length") == null){
-            params.put("length","15");
+        if (params.get("length") == null) {
+            params.put("length", "15");
         }
         DataTableResult result = taskService.getTaskList(params);
         return JsonTool.obj2json(result);
+    }
+
+    @RequestMapping("/upload.do")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        String fileType = fileName.substring(fileName.lastIndexOf("."));
+        String taskId = IdTool.getUUID();
+        File targetFile = new File("task_files");
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(targetFile + File.separator + taskId + fileType);
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return JsonTool.makeResultJson(false, e.getMessage());
+        }
+        return JsonTool.makeResultJson(true, taskId);
     }
 }
--- src/main/resources/static/tc/js/main.js
+++ src/main/resources/static/tc/js/main.js
@@ -1,21 +1,27 @@
 var loginUserInfo;
 //是否为测试模式
 var isTest = true;
-if(isTest){
-    loginUserInfo ={"userId":"0","username":"admin","password":null,"createTime":null,"dept":{"deptId":"0","deptName":"管理员","sign":"admin","createTime":null}};
-    $("#loginUserInfo_span").text("你好，"+loginUserInfo.username+"("+loginUserInfo.dept.deptName+")");
+if (isTest) {
+    loginUserInfo = {
+        "userId": "0",
+        "username": "admin",
+        "password": null,
+        "createTime": null,
+        "dept": {"deptId": "0", "deptName": "管理员", "sign": "admin", "createTime": null}
+    };
+    $("#loginUserInfo_span").text("你好，" + loginUserInfo.username + "(" + loginUserInfo.dept.deptName + ")");
     loadTaskTableData(false);
     $("#createTask_btn").show();
-}else {
+} else {
     $.ajax({
-        url:"/tc/loginUserInfo.do",
-        type:"post",
-        dataType:"json",
-        success:function(responseData){
+        url: "/tc/loginUserInfo.do",
+        type: "post",
+        dataType: "json",
+        success: function (responseData) {
             loginUserInfo = responseData;
             //初始化顶部登录用户信息
-            $("#loginUserInfo_span").text("你好，"+loginUserInfo.username+"("+loginUserInfo.dept.deptName+")");
-            if(loginUserInfo.dept.sign === 'admin'){
+            $("#loginUserInfo_span").text("你好，" + loginUserInfo.username + "(" + loginUserInfo.dept.deptName + ")");
+            if (loginUserInfo.dept.sign === 'admin') {
                 $("#createTask_btn").show();
             }
             loadTaskTableData(false);
@@ -23,19 +29,20 @@ if(isTest){
     });
 }
 
-$("#logout_btn").on("click",function(){
-    if(confirm("确定要注销吗？")){
+$("#logout_btn").on("click", function () {
+    if (confirm("确定要注销吗？")) {
         $.ajax({
-            url:"/tc/logout.do",
-            success:function(){
+            url: "/tc/logout.do",
+            success: function () {
                 location.href = "login.html";
             }
         });
     }
 });
 
-function loadTaskTableData(isSearch){
-    if($.fn.dataTable.isDataTable('#task_table')){
+//加载任务列表
+function loadTaskTableData(isSearch) {
+    if ($.fn.dataTable.isDataTable('#task_table')) {
         $('#task_table').DataTable().destroy();
     }
     $('#task_table').dataTable({
@@ -45,14 +52,14 @@ function loadTaskTableData(isSearch){
         'bProcessing': true,
         //分页
         'paging': true,
-        'bStateSave' : false,
+        'bStateSave': false,
         'sScrollY': '503px', //支持垂直滚动
         //从服务器处理分页
         'bServerSide': true,
         //查询请求action url
         'ajax': {
             'url': '/tc/task/list.do',
-            'type':'post',
+            'type': 'post',
             'dataSrc': 'list'
         },
         // 客户端传给服务器的查询参数为sSearch,服务端根据条件查出数据源即可
@@ -60,66 +67,134 @@ function loadTaskTableData(isSearch){
         //本地搜索
         'searching': false,
         //每页显示多少条数据
-        'lengthChange':false,
+        'lengthChange': false,
         //每页显示数量：15条记录
         'iDisplayLength': 10,
-        'bInfo' : true, //是否显示页脚信息，DataTables插件左下角显示记录数
+        'bInfo': true, //是否显示页脚信息，DataTables插件左下角显示记录数
         'sPaginationType': 'full_numbers', //详细分页组，可以支持直接跳转到某页
         'columns': [
-            {'data': 'taskId','width':'5%',render:function(data){
-                return "&nbsp;&nbsp;<input type='checkbox' name='table_ids' value='"+data+"'>";
-            }},
-            {'data': 'taskName','width':'20%',render:function(data){
-                return "&nbsp;&nbsp;"+data;
-            }},
-            {'data': 'type','width':'10%',render:function(data){
+            {
+                'data': 'taskId', 'width': '5%', render: function (data) {
+                return "&nbsp;&nbsp;<input type='checkbox' name='table_ids' value='" + data + "'>";
+            }
+            },
+            {
+                'data': 'taskName', 'width': '20%', render: function (data) {
+                return "&nbsp;&nbsp;" + data;
+            }
+            },
+            {
+                'data': 'type', 'width': '10%', render: function (data) {
                 var html;
-                if("excel" === data){
+                if ("excel" === data) {
                     html = "Excel表格";
-                }else if("word" === data){
+                } else if ("word" === data) {
                     html = "Word文档";
-                }else{
+                } else {
                     html = "未知类型";
                 }
-                return "&nbsp;&nbsp;"+html;
-            }},
-            {'data': 'isFinish','width':'10%',render:function(data){
+                return "&nbsp;&nbsp;" + html;
+            }
+            },
+            {
+                'data': 'isFinish', 'width': '10%', render: function (data) {
                 var html;
-                if(data === 0){
+                if (data === 0) {
                     html = "<font color='#f00'>未完成</font>";
-                }else{
+                } else {
                     html = "<font color='#00aa00'>已完成</font>";
                 }
-                return "&nbsp;&nbsp;"+html;
-            }},
-            {'data': 'createTime','width':'18%',render:function(data){
+                return "&nbsp;&nbsp;" + html;
+            }
+            },
+            {
+                'data': 'createTime', 'width': '18%', render: function (data) {
                 var html;
                 var date = new Date(data);
-                html = date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日 "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-                return "&nbsp;&nbsp;"+html;
-            }},
-            {'data': 'createUser','width':'12%',render:function(data){
-                return "&nbsp;&nbsp;"+data.username+"("+data.dept.deptName+")";
-            }},
-            {'data': 'progress','width':'6%',render:function(data){
-                return "&nbsp;&nbsp;"+data;
-            }},
-            {'data': 'isFinish','width':'19%',render:function(data){
+                html = date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日 " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+                return "&nbsp;&nbsp;" + html;
+            }
+            },
+            {
+                'data': 'createUser', 'width': '12%', render: function (data) {
+                return "&nbsp;&nbsp;" + data.username + "(" + data.dept.deptName + ")";
+            }
+            },
+            {
+                'data': 'progress', 'width': '6%', render: function (data) {
+                return "&nbsp;&nbsp;" + data;
+            }
+            },
+            {
+                'data': 'isFinish', 'width': '19%', render: function (data) {
                 var html = "&nbsp;&nbsp;";
-                if(loginUserInfo.dept.sign === 'admin'){
+                if (loginUserInfo.dept.sign === 'admin') {
                     html += "<button class='progress_btn'>详细进度</button><button class='progress_btn'>查看规则</button>";
-                }else if(data === 0){
+                } else if (data === 0) {
                     html += "<button class='progress_btn'>填写</button>";
                 }
                 return html;
-            }}
+            }
+            }
         ],
         //语言
         'language': {
             url: 'internation/message_zh_CN.txt'
         },
         //排序
-        'sort':false,
+        'sort': false,
         'aaSorting': []
     });
 }
+
+//发布任务按钮事件
+$("#createTask_btn").on("click", function () {
+    creatTaskModalInit();
+    $("#createTaskModal").modal("show");
+});
+
+function creatTaskModalInit(){
+    $("#createTask_file_name").html("");
+    $("#createTask_file_size").html("");
+    $("#createTask_file_progress").html("");
+    $("#createTask_file").attr("disabled", false);
+    $("#next_btn").attr("disabled", true);
+}
+
+//发布任务-上传事件
+$('#createTask_file').fileupload({
+    url: "/tc/task/upload.do",
+    dataType: "json",
+    add: function (e, data) {
+        var fileName = data.originalFiles[0].name;
+        var fileType = fileName.substring(fileName.lastIndexOf(".") + 1);
+        if (fileType === "doc" || fileType === "docx" || fileType === "xls" || fileType === "xlsx") {
+            $("#createTask_file_name").html(fileName);
+            $("#createTask_file_size").html("<label>大小：</label>" + tools.fileSizeOf(data.originalFiles[0].size));
+            data.submit();
+        } else {
+            $("#createTask_file_name").text("任务文件格式应为doc、docx、xls、xlsx");
+            $("#createTask_file_size").html("");
+            return false;
+        }
+    },
+    progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $("#createTask_file_progress").html("，<label>进度：</label>" + progress + "%");
+    },
+    done: function (e, data) {
+        $("#createTask_file_progress").html("，<label>进度：</label>已上传完成");
+        $("#createTask_file").hide();
+        $("#next_btn").attr("disabled", false);
+    }
+});
+
+var tools = {
+    fileSizeOf: function (size) {
+        if (size >= 1024 * 1024) {
+            return (size / (1024 * 1024)).toFixed(2) + "mb";
+        } else {
+            return (size / 1024).toFixed(0) + "kb";
+        }
+    }
+};
